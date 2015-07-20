b446c8fa44c3c78a4a2987b367f83bef83affc5f
Bryan Oakley <bryanoakley@echo.com>
2015-07-20 15:05:03 -0500

    Keyword now inherits from RobotStatements rather than Testcase

--- rflint/parser/rfkeyword.py
+++ rflint/parser/rfkeyword.py
@@ -1,14 +1,21 @@
 # Does this need to be in its own file, or can I combine 
 # testcase and keyword into one file? 
-from testcase import Testcase
+from common import RobotStatements
 
-class Keyword(Testcase):
+class Keyword(RobotStatements):
     '''A robotframework keyword
 
     A keyword is identical to a testcase in almost all respects
     except for some of the metadata it supports (which this definition
     doesn't (yet) account for...).
     '''
+    def __init__(self, parent, linenumber, name):
+        RobotStatements.__init__(self)
+        self.linenumber = linenumber
+        self.name = name
+        self.rows = []
+        self.parent = parent
+
     def __repr__(self):
         # should this return the fully qualified name?
         return "<Keyword: %s>" % self.name
