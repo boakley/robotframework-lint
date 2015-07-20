53425e2b16cd5a9d4e6daa6846c9e680d6515ad6
Bryan Oakley <bryanoakley@echo.com>
2015-07-20 15:06:31 -0500

    added new class SimpleTableMixin

--- rflint/parser/tables.py
+++ rflint/parser/tables.py
@@ -1,3 +1,5 @@
+from common import Statement
+
 class RobotTable(object):
     '''A table made up of zero or more rows'''
     def __init__(self, parent, linenumber=0, name=None):
@@ -23,9 +25,51 @@ class RobotTable(object):
     def __repr__(self):
         return "<%s(linenumbmer=%s, name=\"%s\")>" % (self.__class__.__name__, self.linenumber, self.name)
 
+class SimpleTableMixin(object):
+    '''Mixin to handle simple tables (tables other than keywords and tests)'''
+
+    @property
+    def statements(self):
+        '''Return a list of statements
+
+        This is done by joining together any rows that
+        have continuations
+        '''
+        # FIXME: no need to do this every time; we should cache the
+        # result
+        if len(self.rows) == 0:
+            return []
+
+        current_statement = Statement(self.rows[0])
+        current_statement.startline = self.rows[0].linenumber
+        current_statement.endline = self.rows[0].linenumber
+        statements = []
+        for row in self.rows[1:]:
+            if len(row) > 0 and row[0] == "...":
+                # we found a continuation
+                current_statement += row[1:]
+                current_statement.endline = row.linenumber
+            else:
+                if len(current_statement) > 0:
+                    # append current statement to the list of statements...
+                    statements.append(current_statement)
+                # start a new statement
+                current_statement = Statement(row)
+                current_statement.startline = row.linenumber
+                current_statement.endline = row.linenumber
+
+        if len(current_statement) > 0:
+            statements.append(current_statement)
+
+        # trim trailing blank statements
+        while (len(statements[-1]) == 0 or 
+               ((len(statements[-1]) == 1) and len(statements[-1][0]) == 0)):
+            statements.pop()
+        return statements
+
 class DefaultTable(RobotTable): pass  # the table with no name
-class UnknownTable(RobotTable): pass  # a table with an 
-class SettingTable(RobotTable): pass
+class UnknownTable(RobotTable): pass  # a table with an unknown header
+class SettingTable(RobotTable, SimpleTableMixin): pass
 class VariableTable(RobotTable): pass
 class MetadataTable(RobotTable): pass
 
