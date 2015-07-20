5a843fcb38d0d44739185369c564fad5a156725b
Bryan Oakley <bryanoakley@echo.com>
2015-07-20 15:04:04 -0500

    added RobotStatements class

--- rflint/parser/common.py
+++ rflint/parser/common.py
@@ -1,4 +1,66 @@
 import re
+
+class RobotStatements(object):
+    def append(self, linenumber, raw_text, cells):
+        """Add another row of data from a test suite"""
+        self.rows.append(Row(linenumber, raw_text, cells))
+
+    @property
+    def steps(self):
+        '''Return a list of steps (statements that are not settings or comments)'''
+        steps = []
+        for statement in self.statements:
+            if ((not statement.is_comment()) and 
+                (not statement.is_setting())):
+                steps.append(statement)
+        return steps
+
+    @property
+    def settings(self):
+        '''Return a list of settings (statements with cell[1] matching \[.*?\])
+
+        Note: this returns any statement that *looks* like a setting. If you have
+        a misspelled or completely bogus setting, it'll return that too
+        (eg: | | [Blockumentation] | hello, world)
+        '''
+        return [statement for statement in self.statements if statement.is_setting()]
+
+    @property
+    def statements(self):
+        '''Return a list of statements
+
+        This is done by joining together any rows that
+        have continuations
+        '''
+        # FIXME: no need to do this every time; we should cache the
+        # result
+        if len(self.rows) == 0:
+            return []
+
+        current_statement = Statement(self.rows[0])
+        current_statement.startline = self.rows[0].linenumber
+        current_statement.endline = self.rows[0].linenumber
+        statements = []
+        for row in self.rows[1:]:
+            if len(row) > 1 and row[0] == "" and row[1] == "...":
+                # we found a continuation
+                current_statement += row[2:]
+                current_statement.endline = row.linenumber
+            else:
+                if len(current_statement) > 0:
+                    # append current statement to the list of statements...
+                    statements.append(current_statement)
+                # start a new statement
+                current_statement = Statement(row)
+                current_statement.startline = row.linenumber
+                current_statement.endline = row.linenumber
+
+        if len(current_statement) > 0:
+            statements.append(current_statement)
+
+        return statements
+
+
 # TODO: make Row and Statement more similar -- either
 # both should inherit from list, or neither should. 
 class Row(object):
@@ -19,6 +81,8 @@ class Row(object):
         return self.cells[key]
     def __repr__(self):
         return "<line: %s cells: %s>" % (self.linenumber, str(self.cells))
+    def __contains__(self, key):
+        return key in self.cells
 
 class Comment(Row):
     # this isn't entirely correct or well thought out. 
