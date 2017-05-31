3afe92149e79121f101997d691762dc1000a1ccc
Bryan Oakley <bryan.oakley@draft2digital.com>
2017-05-31 14:07:41 -0500

    fixes #41 - is_comment was returning True more often than it should

--- rflint/parser/common.py
+++ rflint/parser/common.py
@@ -111,11 +111,17 @@ class Statement(list):
 
     def is_comment(self):
         '''Return True if the first non-empty cell starts with "#"'''
+
         for cell in self[:]:
             if cell == "":
                 continue
+
+            # this is the first non-empty cell. Check whether it is
+            # a comment or not.
             if cell.lstrip().startswith("#"):
                 return True
+            else:
+                return False
         return False
 
     def __repr__(self):
