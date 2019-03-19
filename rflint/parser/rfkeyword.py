fa80182396cdc0d66d9ce19b1fd5fad6ee18663d
Fredy P <fredy.pulido@savoirfairelinux.com>
2019-03-19 14:42:09 -0400

    keyword: add tags function
    This commit copy-pastes the `tags` function from testcase.py

--- rflint/parser/rfkeyword.py
+++ rflint/parser/rfkeyword.py
@@ -20,3 +20,12 @@ class Keyword(RobotStatements):
         # should this return the fully qualified name?
         return "<Keyword: %s>" % self.name
 
+    # this is great, except that we don't return the line number
+    # or character position of each tag. The linter needs that. :-(
+    @property
+    def tags(self):
+        tags = []
+        for statement in self.statements:
+            if len(statement) > 2 and statement[1].lower() == "[tags]":
+                tags = tags + statement[2:]
+        return tags
